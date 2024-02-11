6cad7dbfea9b5a735d5c06e0071c057c5959a821
Gamora00 <146001031+Gamora00@users.noreply.github.com>
2024-02-11 22:11:37 +0800

    Add files via upload

---
+++ valentines/script.js
@@ -0,0 +1,11 @@
+
+const yesBtn = document.querySelector(".yes")
+const noBtn = document.querySelector(".no")
+
+
+yesBtn.addEventListener('click', ()=>{
+    window.location.href = "/valentines/yes.html"
+})
+
+
+
---
+++ valentines/yes.js
@@ -0,0 +1,6 @@
+
+
+    const next = document.querySelector('.btn')
+    next.addEventListener('click', ()=>{
+        window.location.href = '/valentines/flowers.html'
+    })
